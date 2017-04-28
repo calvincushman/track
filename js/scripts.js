5cb05402cd6f9eb2061192574f22b1b9d97ffe9b
Calvin Cushman <calvincushman@gmail.com>
2017-04-28 16:56:48 -0700

    fix reference error and add name placement funtion

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,34 @@
 $(function() {
-  $("#questions").submit(function(event) {
+  $(".btn").click(function() { debugger;
     var name = $("input#name").val();
-    var radioOne = $(parseInt("input:radio[name=education]:checked")).val();
-    var radioTwo = $(parseInt("input:radio[name=classes]:checked")).val();
-    var radioThree = $(parseInt("input:radio[name=experience]:checked")).val();
-    var radioFour = $(parseInt("input:radio[name=hobbies]:checked")).val();
-    var radioFive = $(parseInt("input:radio[name=carreer]:checked")).val();
+    var radioOne = $("input:radio[name=education]:checked").val();
+    var radioTwo = $("input:radio[name=classes]:checked").val();
+    var radioThree = $("input:radio[name=experience]:checked").val();
+    var radioFour = $("input:radio[name=hobbies]:checked").val();
+    var radioFive = $("input:radio[name=carreer]:checked").val();
 
-    var score = (radioOne+radioTwo+radioThree+radiradioFour+radioFive)
+    var one = parseInt(radioOne);
+    var two = parseInt(radioTwo);
+    var three = parseInt(radioThree);
+    var four = parseInt(radioFour);
+    var five = parseInt(radioFive);
+
+    var score = (one+two+three+four+five)
+
+    $(".name").text(name);
 
     if(score > 6)
-      $(".css").show
+      $(".css").show();
     if(score < 11 || score > 5)
-      $(".c#").show
+      $(".c#").show();
     if(score < 16 || score > 10)
-      $(".ruby").show
+      $(".ruby").show();
     if(score > 15)
-      $(".php").show
+      $(".php").show();
+
+
+
+
 
     event.preventDefault();
   });
