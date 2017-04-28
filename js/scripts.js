128fb2613c35b5b9ff7f92ffadfb527cf48e73ed
Calvin Cushman <calvincushman@gmail.com>
2017-04-28 16:12:09 -0700

    add if statements for evaluating score

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@ $(function() {
     var radioFive = $(parseInt("input:radio[name=carreer]:checked")).val();
 
     var score = (radioOne+radioTwo+radioThree+radiradioFour+radioFive)
+
+    if(score > 6)
+      $(".css").show
+    if(score < 11 || score > 5)
+      $(".c#").show
+    if(score < 16 || score > 10)
+      $(".ruby").show
+    if(score > 15)
+      $(".php").show
+
     event.preventDefault();
   });
 });
